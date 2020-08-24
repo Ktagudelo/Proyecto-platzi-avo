aa7bb7c248939f4a980925dc1e907ea4394b98fd
ktagudelo <ospinacatalina2@gmail.com>
2020-08-24 01:07:55 -0500

    Proyecto platzi-avo

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch';
+import Layout from '@components/Layout/Layout'
+import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import ProductList from '@components/ProductList/ProductList'
+
+export const getServerSideProps = async () => {
+  const response = await fetch('https://platzi-avo.vercel.app/yes-or-no')
+    const {data: productList}: TAPIAvoResponse = await response.json()
+
+  return {
+    props: {
+      productList,
+    }
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
+  return (
+    <Layout>
+      <KawaiiHeader />
+      <ProductList products={productList} />
+    </Layout>
+  )
+}
+
+export default HomePage
